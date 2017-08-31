7f68a060c635bc3e02ed5097362c54d29308bbb3
emilymadeline <emilydick60@gmail.com>
2017-08-31 20:33:47 +1200

    each post a unique id

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var saveNewPost = function (request, response) {
   post.image = request.body.image;
   post.author = request.body.author;
   post.time = new Date();
+  post.id = Math.round(Math.random() * 10000);
   posts.push(post);
   response.send("thanks for your message. Press back to add another");
 }
@@ -37,3 +38,12 @@ app.post('/posts', saveNewPost);
 //listen for connections on port 3000
 app.listen(3000);
 console.log("Hi! I am listening at http://localhost:3000");
+
+//find a post with given id
+
+app.get('/post', function (req, res) {
+   var searchId = req.query.id;
+   console.log("Searching for post " + searchId);
+   var post = posts.find(x => x.id == searchId);
+   res.send(post);
+});
